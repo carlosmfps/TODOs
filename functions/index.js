26d4904598a2792f9fa5f157531cd061600fd2a0
soularac <cmfpsantiago@inf.ufrgs.br>
2020-12-03 22:27:31 -0300

    finish auth and start front end

--- functions/index.js
+++ functions/index.js
@@ -14,17 +14,22 @@ const {
 const {
     loginUser,
     signUpUser,
-    uploadProfilePhoto
+    uploadProfilePhoto,
+    getUserDetail,
+    updateUserDetails
 } = require('./APIs/users')
 
 //Todos
-app.post('/todo', postOneTodo);
-app.get('/todos', getAllTodos);
-app.delete('/todo/:todoId', deleteTodo);
-app.put('/todo/:todoId', editTodo);
-exports.api = functions.https.onRequest(app);
+app.post('/todo', auth, postOneTodo);
+app.get('/todos', auth, getAllTodos);
+app.delete('/todo/:todoId', auth, deleteTodo);
+app.put('/todo/:todoId', auth, editTodo);
 
 //Users
 app.post('/login', loginUser);
 app.post('/signup', signUpUser);
 app.post('/user/image', auth, uploadProfilePhoto);
+app.get('/user', auth, getUserDetail);
+app.post('/user', auth, updateUserDetails);
+
+exports.api = functions.https.onRequest(app);
