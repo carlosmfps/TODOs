42ccf299db14fd6ec27924e037f4f381b4a93bc4
soularac <cmfpsantiago@inf.ufrgs.br>
2021-01-02 14:13:32 -0300

    add home page and signup

--- view/src/App.js
+++ view/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import login from './pages/login';
+import signup from './pages/signup';
+import home from './pages/home';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
         <div>
           <Switch>
             <Route exact path="/login" component={login} />
+            <Route exact path="/signup" component={signup}/>
+            <Route exact path="/" component={home}/>
           </Switch>
         </div>
       </Router>
